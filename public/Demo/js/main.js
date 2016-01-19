19774fec9ed4df13e9cc289f91d36689c8cceeec
Sarah Maris <sarah.87.maris@gmail.com>
2016-01-19 15:45:08 -0500

    Remove unneeded CSS

--- public/Demo/js/main.js
+++ public/Demo/js/main.js
@@ -33,16 +33,16 @@
 // STiCKY NAV based on http://www.jay-han.com/2011/11/10/simple-smart-sticky-navigation-bar-with-jquery/
 
 $(document).ready(function() {
-	
+
 	//calculate height of header
 	var headerHeight = $('header').outerHeight();
-console.log("heifht", headerHeight)
-	//add padding to main 
+
+	//add padding to main
 	$(main).css('padding-top', headerHeight +'px');
 
 	//Set height to change menu
 	var changeHeight = 300;
-	
+
 	//Set minder
 	var isSmall = false;
 
@@ -51,36 +51,36 @@ console.log("heifht", headerHeight)
 
 		//if scrolled down more than the header’s height fixed
 		if ($(window).scrollTop() > changeHeight ){
-		console.log("HERE")	
+
 			//check to see if already small
 			if (!isSmall) {
-console.log("is small?", isSmall)
+
 				//set minder to true
 				isSmall = true;
 
 				//add "small" class to the <nav>
 				$('header').addClass('small');
-				
+
 				//re-calculate height of header
 				headerHeight = $('header').outerHeight();
 
-				//add padding to main 
-				$(main).css('padding-top', headerHeight +'px');	
-				
+				//add padding to main
+				$(main).css('padding-top', headerHeight +'px');
+
 		}
-				
-		// when scroll up or less than aboveHeight process	
+
+		// when scroll up or less than aboveHeight process
 		} else {
-			
+
 			//check to see if small
 			if (isSmall) {
 
 			//set minder to false
 				isSmall = false;
-				
+
 				// remove the "small" class
 				$('header').removeClass('small');
-				
+
 				//re-calculate height of header
 				headerHeight = $('header').outerHeight();
 
@@ -88,14 +88,15 @@ console.log("is small?", isSmall)
 				$(main).css('padding-top', headerHeight +'px');
 
 
-			}	
+			}
 		}
-			
-			
+
+
 	});
 
-			$( ".sf-menu li" ).click(function() {
-  console.log( "Handler for .click() called." );
-          drawer.classList.remove('open');
-});
+	//Add click event to close menu when item is clicked in hamburger mode
+	$( ".sf-menu li" ).click(function() {
+		drawer.classList.remove('open');
+	});
+
 });
